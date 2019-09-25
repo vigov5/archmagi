990075a1b2e6f82b5136ccbcc0eade1d6268c721
Nguyen Anh Tien <nguyen.anh.tien@framgia.com>
2019-09-25 17:10:01 +0700

    Add more cards

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -30,7 +30,24 @@ cards = [
   {"Fort", "+20 to tower", [bricks: 18], [{:me, {:add, :tower, 20}}]},
   {"Babylon", "+32 to tower", [bricks: 39], [{:me, {:add, :tower, 32}}]},
   {"Archer", "2 damge", [recruits: 1], [{:you, {:attack_wall, 2}}]},
-  {"Knight", "3 damge", [recruits: 2], [{:you, {:attack_wall, 3}}]}
+  {"Knight", "3 damge", [recruits: 2], [{:you, {:attack_wall, 3}}]},
+  {"Rider", "4 damge", [recruits: 2], [{:you, {:attack_wall, 4}}]},
+  {"Platoon", "6 damge", [recruits: 4], [{:you, {:attack_wall, 6}}]},
+  {"Recurit", "+1 to dungeon", [recruits: 8], [{:me, {:add, :dungeon, 1}}]},
+  {"Catapult", "12 damage", [recruits: 10], [{:you, {:attack_wall, 12}}]},
+  {"Saboteur", "-4 enemy stocks", [recruits: 12], [{:you, {:sub, :bricks, 4}}, {:you, {:sub, :gems, 4}}, {:you, {:sub, :recruits, 4}}]},
+  {"Clash", "-10 to enemy tower", [recruits: 18], [{:you, {:tower, 10}}]},
+  {"Orc", "32 damage", [recruits: 28], [{:you, {:attack_wall, 32}}]},
+  {"Brick Pile", "+8 to bricks", [gems: 4], [{:me, {:add, :bricks, 8}}]},
+  {"Crystal Mine", "+8 to gems", [gems: 4], [{:me, {:add, :gems, 8}}]},
+  {"Weapon Tech", "+8 to recruits", [gems: 4], [{:me, {:add, :recruits, 8}}]},
+  {"Fake Brick", "-8 to enemy bricks", [gems: 4], [{:you, {:sub, :bricks, 8}}]},
+  {"Alchemist Failure", "-8 to enemy gems", [gems: 4], [{:you, {:sub, :gems, 8}}]},
+  {"Drop Weapon", "-8 to enemy recruits", [gems: 4], [{:you, {:sub, :recruits, 8}}]},
+  {"Sorcerer", "+1 to magic", [gems: 8], [{:me, {:add, :magic, 1}}]},
+  {"Dragon", "25 damage", [gems: 21], [{:you, {:attack_wall, 25}}]},
+  {"Pixies", "+22 to tower", [gems: 22], [{:me, {:add, :tower, 22}}]},
+  {"Curse", "+1 to all, -1 to all enemies", [gems: 45], [{:you, {:sub, :bricks, 1}}, {:you, {:sub, :gems, 1}}, {:you, {:sub, :recruits, 1}}, {:you, {:sub, :quarry, 1}}, {:you, {:sub, :magic, 1}}, {:you, {:sub, :dungeon, 1}}, {:you, {:sub, :wall, 1}}, {:you, {:sub, :tower, 1}}, {:me, {:add, :bricks, 1}}, {:me, {:add, :gems, 1}}, {:me, {:add, :recruits, 1}}, {:me, {:add, :quarry, 1}}, {:me, {:add, :magic, 1}}, {:me, {:add, :dungeon, 1}}, {:me, {:add, :wall, 1}}, {:me, {:add, :tower, 1}}]},
 ]
 
 cards
